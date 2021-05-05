260e0076a368fee2c62ae014c97d9b1dcaf1a62e
WongLeyF <jumawongleyf@ittepic.edu.mx>
2021-05-05 02:33:15 -0600

    feat: add button to redirect to activity 2

--- app/src/main/java/mx/tecnm/tepic/ladm_u2_practica2_topos/MainActivity.kt
+++ app/src/main/java/mx/tecnm/tepic/ladm_u2_practica2_topos/MainActivity.kt
@@ -1,11 +1,16 @@
 package mx.tecnm.tepic.ladm_u2_practica2_topos
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun goToAnotherActivity(view: View?) {
+        startActivity(Intent(this, MainActivity2::class.java))
     }
 }
